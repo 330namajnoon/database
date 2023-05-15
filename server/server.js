78fc6e7802468aabc351eb0b1701d81b8bb31862
30namajnoon <sina.majnoonhjk@gmail.com>
2023-05-16 01:21:08 +0200

    1

--- server/server.js
+++ server/server.js
@@ -3,55 +3,58 @@ const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 const fs = require("fs");
+const path = require("path");
+const pdp = path.join(__dirname, "./");
 const port = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
+app.use(express.static(pdp));
 const server = http.createServer(app);
-server.listen(port , ()=> {
+server.listen(port, () => {
     console.log(`server is up on port ${port}!`);
 })
 
-app.post("/login",multer().none(),(req,res)=> {
-    fs.readFile(`./database/users.json`,(err,data)=> {
-        if(err) throw err;
+app.post("/login", multer().none(), (req, res) => {
+    fs.readFile(`./database/users.json`, (err, data) => {
+        if (err) throw err;
         let users = JSON.parse(data.toString());
         let user = JSON.parse(req.body.data);
         let t = false;
         users.forEach(u => {
-            if(u.userName == user.userName && u.password == user.password) {
-               t = true
+            if (u.userName == user.userName && u.password == user.password) {
+                t = true
             }
         })
-        if(t) {
-            fs.readFile(`./database/${user.userName}/userData.json`,(err,data)=> {
+        if (t) {
+            fs.readFile(`./database/${user.userName}/userData.json`, (err, data) => {
                 let d = JSON.parse(data);
                 res.send(JSON.stringify(d.databases));
             })
-        }else {
+        } else {
             res.send(JSON.stringify(t));
 
         }
     })
-   
+
 })
-app.post("/signup",multer().none(),(req,res)=> {
-    fs.readFile(`./database/users.json`,(err,data)=> {
-        if(err) throw err;
+app.post("/signup", multer().none(), (req, res) => {
+    fs.readFile(`./database/users.json`, (err, data) => {
+        if (err) throw err;
         let users = JSON.parse(data.toString());
         let newUser = JSON.parse(req.body.data);
         let t = false;
         users.forEach(u => {
-            if(u.userName == newUser.userName) t = true;
+            if (u.userName == newUser.userName) t = true;
         })
-        if(t) {
+        if (t) {
             res.send("Este usuario ya existe!");
-        }else {
+        } else {
             users.push(newUser);
-            fs.writeFile(`./database/users.json`,JSON.stringify(users),(err)=> {
-                if(err) throw err;
+            fs.writeFile(`./database/users.json`, JSON.stringify(users), (err) => {
+                if (err) throw err;
                 fs.mkdirSync(`./database/${newUser.userName}`);
                 newUser.databases = [];
-                fs.appendFileSync(`./database/${newUser.userName}/userData.json`,JSON.stringify(newUser));
+                fs.appendFileSync(`./database/${newUser.userName}/userData.json`, JSON.stringify(newUser));
                 res.send(true);
             })
         }
@@ -59,67 +62,163 @@ app.post("/signup",multer().none(),(req,res)=> {
 
 })
 
-app.post("/createDatabase",multer().none(),(req,res)=> {
-    fs.readFile(`./database/${req.body.userName}/userData.json`,(err,data)=> {
-        if(err) throw err;
+app.post("/createDatabase", multer().none(), (req, res) => {
+    fs.readFile(`./database/${req.body.userName}/userData.json`, (err, data) => {
+        if (err) throw err;
         let userData = JSON.parse(data.toString());
         let searche = userData.databases.find(d => d.databaseName == req.body.databaseName);
-        if(!searche){
+        if (!searche) {
 
-            userData.databases.push({databaseName:req.body.databaseName,tables:[]});
-            fs.writeFile(`./database/${req.body.userName}/userData.json`,JSON.stringify(userData),(err)=> {
-                fs.mkdirSync(`./database/${req.body.userName+'/'+req.body.databaseName}`);
+            userData.databases.push({ databaseName: req.body.databaseName, tables: [] });
+            fs.writeFile(`./database/${req.body.userName}/userData.json`, JSON.stringify(userData), (err) => {
+                fs.mkdirSync(`./database/${req.body.userName + '/' + req.body.databaseName}`);
                 res.send(JSON.stringify(userData.databases));
             })
-        }else {
+        } else {
             res.send(JSON.stringify(false));
         }
-     
+
     })
-   
+
 })
-app.post("/createTable",multer().none(),(req,res)=> {
-    fs.readFile(`./database/${req.body.userName}/userData.json`,(err,data)=> {
-        if(err) throw err;
+app.post("/createTable", multer().none(), (req, res) => {
+    fs.readFile(`./database/${req.body.userName}/userData.json`, (err, data) => {
+        if (err) throw err;
         let userData = JSON.parse(data.toString());
         let searche = userData.databases.find(d => d.databaseName == req.body.databaseName);
-        if(searche){
+        if (searche) {
             let tSearch = userData.databases.find(d => d.databaseName == req.body.databaseName).tables.find(t => t.tableName == req.body.tableName);
-            if(!tSearch) {
+            if (!tSearch) {
                 let database = userData.databases.find(d => d.databaseName == req.body.databaseName);
-                database.tables.push({databaseName:req.body.databaseName,tableName:req.body.tableName});
-                fs.writeFile(`./database/${req.body.userName}/userData.json`,JSON.stringify(userData),(err)=> {
-                    fs.appendFileSync(`./database/${req.body.userName}/${req.body.databaseName}/${req.body.tableName}.json`,JSON.stringify([]));
+                database.tables.push({ databaseName: req.body.databaseName, tableName: req.body.tableName });
+                fs.writeFile(`./database/${req.body.userName}/userData.json`, JSON.stringify(userData), (err) => {
+                    fs.appendFileSync(`./database/${req.body.userName}/${req.body.databaseName}/${req.body.tableName}.json`, JSON.stringify([]));
                     res.send(JSON.stringify(userData.databases));
                 })
-            }else {
+            } else {
                 res.send(JSON.stringify(false));
             }
-           
-           
-        }else {
+
+
+        } else {
             res.send(JSON.stringify(false));
         }
-     
+
     })
-   
+
 })
 
-app.post("/uploadData",multer().none(),(req,res)=> {
-    fs.readFile(`./database/${req.body.userName+'/'+req.body.databaseName+'/'+req.body.tableName}.json`,(err,data)=> {
-        if(err) res.send(JSON.stringify(false));
+app.post("/uploadData", multer().none(), (req, res) => {
+    fs.readFile(`./database/${req.body.userName + '/' + req.body.databaseName + '/' + req.body.tableName}.json`, (err, data) => {
+        if (err) res.send(JSON.stringify(false));
         let tableData = JSON.parse(data.toString());
-        tableData.push(JSON.parse(req.body.data));
-        console.log(tableData)
-        fs.writeFile(`./database/${req.body.userName+'/'+req.body.tableName}.json`,JSON.stringify(tableData),(err)=> {
-            if(err) res.send(JSON.stringify(false));
-            res.send('se ha guardado correctamente!');
+        let newData;
+        if(tableData.length > 0 ) {
+            newData = {id:tableData[tableData.length-1].id+1,data:JSON.parse(req.body.data)};
+        }else {
+            newData = {id:1,data:JSON.parse(req.body.data)};
+        }
+        tableData.push(newData);
+
+        fs.writeFile(`./database/${req.body.userName + '/' + req.body.databaseName + '/' + req.body.tableName}.json`, JSON.stringify(tableData), (err) => {
+            if (err) res.send(JSON.stringify(false));
+            res.send(JSON.stringify(tableData));
         })
     })
 })
-app.post("/downloadData",multer().none(),(req,res)=> {
-    fs.readFile(`./database/${req.body.userName+'/'+req.body.tableName}.json`,(err,data)=> {
-        if(err) res.send(JSON.stringify(false));
+app.post("/deleteData", multer().none(), (req, res) => {
+    fs.readFile(`./database/${req.body.userName + '/' + req.body.databaseName + '/' + req.body.tableName}.json`, (err, data) => {
+        if (err) res.send(JSON.stringify(false));
+        fs.writeFile(`./database/${req.body.userName + '/' + req.body.databaseName + '/' + req.body.tableName}.json`, JSON.stringify([]), (err) => {
+            if (err) res.send(JSON.stringify(false));
+            res.send('se ha borrado los datos correctamente!');
+        })
+    })
+})
+app.post("/deleteDataById", multer().none(), (req, res) => {
+    fs.readFile(`./database/${req.body.userName + '/' + req.body.databaseName + '/' + req.body.tableName}.json`, (err, data) => {
+        if (err) res.send(JSON.stringify(false));
+        let newdata = JSON.parse(data.toString()).filter(d => d.id !== parseInt(req.body.id));
+        fs.writeFile(`./database/${req.body.userName + '/' + req.body.databaseName + '/' + req.body.tableName}.json`, JSON.stringify(newdata), (err) => {
+            if (err) res.send(JSON.stringify(false));
+            res.send(JSON.stringify(newdata));
+        })
+    })
+})
+app.post("/deleteTable", multer().none(), (req, res) => {
+
+    fs.readFile(`./database/${req.body.userName}/userData.json`, (err, data) => {
+        if (err) throw err;
+        let userData = JSON.parse(data.toString());
+        userData.databases = userData.databases.map(d => {
+            let database = d;
+            if (d.databaseName == req.body.databaseName) {
+                database.tables = database.tables.filter(t => t.tableName !== req.body.tableName);
+
+            }
+            return database;
+        })
+        fs.writeFile(`./database/${req.body.userName}/userData.json`, JSON.stringify(userData), (err) => {
+            if (err) throw err;
+            try {
+                fs.unlinkSync(`./database/${req.body.userName + '/' + req.body.databaseName + '/' + req.body.tableName}.json`);
+                res.send(JSON.stringify(userData.databases));
+            } catch (error) {
+                res.send(error);
+            }
+        })
+
+    })
+
+
+})
+app.post("/deleteDatabase", multer().none(), (req, res) => {
+    fs.readFile(`./database/${req.body.userName}/userData.json`, (err, data) => {
+        if (err) throw err;
+        let userData = JSON.parse(data.toString());
+        userData.databases = userData.databases.filter(d => d.databaseName !== req.body.databaseName);
+        fs.writeFile(`./database/${req.body.userName}/userData.json`, JSON.stringify(userData), (err) => {
+            if (err) throw err;
+            try {
+                fs.rmSync(`./database/${req.body.userName + '/' + req.body.databaseName}`, { recursive: true, force: true });
+                res.send(JSON.stringify(userData.databases));
+            } catch (error) {
+                res.send(error);
+            }
+        })
+
+    })
+   
+
+})
+app.post("/deleteAccont", multer().none(), (req, res) => {
+    fs.readFile(`./database/users.json`, (err, data) => {
+        if (err) throw err;
+        let users = JSON.parse(data.toString());
+        let user = users.find(u => u.userName == req.body.userName && u.password == req.body.password);
+        if(user) {
+            users = users.filter(u => u.userName !== req.body.userName);
+            fs.writeFile(`./database/users.json`, JSON.stringify(users), (err) => {
+                if (err) throw err;
+                try {
+                    fs.rmSync(`./database/${req.body.userName}`, { recursive: true, force: true });
+                    res.send("se ha borrado correctamente!");
+                } catch (error) {
+                    res.send(error);
+                }
+            })
+        }else {
+            res.send("este usuario no existe!");
+        }
+        
+
+    })
+   
+
+})
+app.post("/downloadData", multer().none(), (req, res) => {
+    fs.readFile(`./database/${req.body.userName + '/' + req.body.databaseName + '/' + req.body.tableName}.json`, (err, data) => {
+        if (err) res.send(JSON.stringify(false));
         res.send(data.toString());
     })
 })
---
+++ server/sinaSQL-main.js
@@ -0,0 +1,307 @@
+
+export default function SinaSQL() {
+    this.hostURL = 'http://localhost:4000/';
+    this.connected = { connected: false, databases: [] };
+    this.userData = { userName: '', password: '' };
+}
+SinaSQL.prototype.getUserData = function () {
+    return this.userData;
+};
+SinaSQL.prototype.getDataBases = function () {
+    return this.connected.databases;
+};
+SinaSQL.prototype.setHostUrl = function (hostURL) {
+    this.hostURL = hostURL;
+};
+SinaSQL.prototype.setDataBases = function (databases) {
+    this.connected.databases = databases;
+};
+
+SinaSQL.prototype.setUserData = function (userData) {
+    this.userData = userData;
+};
+SinaSQL.prototype.connect = function (userName, password) {
+    var _this = this;
+    return new Promise(function (resolve) {
+        var http = new XMLHttpRequest();
+        var formData = new FormData();
+        var userData = {
+            userName: userName,
+            password: password
+        };
+        formData.append('data', JSON.stringify(userData));
+        http.open('POST', _this.hostURL + 'login', true);
+        var c = _this.connected;
+        var setUserData = _this.setUserData.bind(_this);
+        http.onreadystatechange = function () {
+            if (http.readyState == 4 && http.status == 200) {
+                if (JSON.parse(http.responseText)) {
+                    c.connected = true;
+                    c.databases = JSON.parse(http.responseText);
+                    setUserData(userData);
+                }
+                else {
+                    c.connected = false;
+                }
+                resolve(c.connected);
+            }
+        };
+        http.send(formData);
+    });
+};
+SinaSQL.prototype.signup = function (userName = "", email = "", password = "") {
+    if(userName&&email&&password) {
+        var _this = this;
+        return new Promise(function (resolve) {
+            var http = new XMLHttpRequest();
+            var formData = new FormData();
+            var newUser = {
+                userName: userName,
+                email: email,
+                password: password
+            };
+            formData.append('data', JSON.stringify(newUser));
+            http.open('POST', _this.hostURL + 'signup', true);
+            http.onreadystatechange = function () {
+                if (http.readyState == 4 && http.status == 200) {
+                    var res = userName;
+                    http.responseText == 'true' ? localStorage.setItem('user', userName) : res = 'este usuario ya existe';
+                    resolve(res);
+                }
+            };
+            http.send(formData);
+        });
+    }else {
+        return false
+    }
+   
+};
+SinaSQL.prototype.uploadData = function (databaseName, tableName, data) {
+    var _this = this;
+    if (this.connected.connected) {
+        return new Promise(function (resolve) {
+            var http = new XMLHttpRequest();
+            var formData = new FormData();
+            formData.append('tableName', tableName);
+            formData.append('databaseName', databaseName);
+            formData.append('data', JSON.stringify(data));
+            formData.append('userName', _this.getUserData().userName);
+            http.open('POST', _this.hostURL + 'uploadData', true);
+            http.onreadystatechange = function () {
+                if (http.readyState == 4 && http.status == 200) {
+                    resolve(http.responseText);
+                }
+            };
+            http.send(formData);
+        });
+    }
+    else {
+        return 'no estas connectado!';
+    }
+};
+SinaSQL.prototype.downloadData = function (databaseName, tableName) {
+    var _this = this;
+    if (this.connected.connected) {
+        return new Promise(function (resolve) {
+            var http = new XMLHttpRequest();
+            var formData = new FormData();
+            formData.append('tableName', tableName);
+            formData.append('databaseName', databaseName);
+            formData.append('userName', _this.getUserData().userName);
+            http.open('POST', _this.hostURL + 'downloadData', true);
+            http.onreadystatechange = function () {
+                if (http.readyState == 4 && http.status == 200) {
+                    resolve(http.responseText);
+                }
+            };
+            http.send(formData);
+        });
+    }
+    else {
+        return 'no estas connectado!';
+    }
+};
+SinaSQL.prototype.deleteData = function (databaseName, tableName) {
+    var _this = this;
+    if (this.connected.connected) {
+        return new Promise(function (resolve) {
+            var http = new XMLHttpRequest();
+            var formData = new FormData();
+            formData.append('tableName', tableName);
+            formData.append('databaseName', databaseName);
+            formData.append('userName', _this.getUserData().userName);
+            http.open('POST', _this.hostURL + 'deleteData', true);
+            http.onreadystatechange = function () {
+                if (http.readyState == 4 && http.status == 200) {
+                    resolve(http.responseText);
+                }
+            };
+            http.send(formData);
+        });
+    }
+    else {
+        return 'no estas connectado!';
+    }
+}
+SinaSQL.prototype.deleteDataById = function (databaseName = "", tableName = "" , id = 1) {
+    var _this = this;
+    if (this.connected.connected) {
+        return new Promise(function (resolve) {
+            var http = new XMLHttpRequest();
+            var formData = new FormData();
+            formData.append('tableName', tableName);
+            formData.append('databaseName', databaseName);
+            formData.append('userName', _this.getUserData().userName);
+            formData.append("id",id);
+            http.open('POST', _this.hostURL + 'deleteDataById', true);
+            http.onreadystatechange = function () {
+                if (http.readyState == 4 && http.status == 200) {
+                    resolve(http.responseText);
+                }
+            };
+            http.send(formData);
+        });
+    }
+    else {
+        return 'no estas connectado!';
+    }
+}
+SinaSQL.prototype.deleteTable = function (databaseName, tableName) {
+    var _this = this;
+    if (this.connected.connected) {
+        return new Promise(function (resolve) {
+            var http = new XMLHttpRequest();
+            var formData = new FormData();
+            formData.append('tableName', tableName);
+            formData.append('databaseName', databaseName);
+            formData.append('userName', _this.getUserData().userName);
+            http.open('POST', _this.hostURL + 'deleteTable', true);
+            http.onreadystatechange = function () {
+                if (http.readyState == 4 && http.status == 200) {
+                    _this.setDataBases(JSON.parse(http.responseText));
+                    resolve(http.responseText);
+                }
+            };
+            http.send(formData);
+        });
+    }
+    else {
+        return 'no estas connectado!';
+    }
+}
+SinaSQL.prototype.deleteDatabase = function (databaseName) {
+    var _this = this;
+    if (this.connected.connected) {
+        return new Promise(function (resolve) {
+            var http = new XMLHttpRequest();
+            var formData = new FormData();
+            formData.append('databaseName', databaseName);
+            formData.append('userName', _this.getUserData().userName);
+            http.open('POST', _this.hostURL + 'deleteDatabase', true);
+
+            http.onreadystatechange = function () {
+                if (http.readyState == 4 && http.status == 200) {
+                    _this.setDataBases(JSON.parse(http.responseText));
+                    resolve(http.responseText);
+                }
+            };
+            http.send(formData);
+        });
+    }
+    else {
+        return 'no estas connectado!';
+    }
+}
+SinaSQL.prototype.deleteAccont = function (userName, password) {
+    var _this = this;
+    return new Promise(function (resolve) {
+        var http = new XMLHttpRequest();
+        var formData = new FormData();
+        formData.append('password', password);
+        formData.append('userName',userName);
+        http.open('POST', _this.hostURL + 'deleteAccont', true);
+        http.onreadystatechange = function () {
+            if (http.readyState == 4 && http.status == 200) {
+                resolve(http.responseText);
+            }
+        };
+        http.send(formData);
+    });
+
+
+}
+SinaSQL.prototype.createDatabase = function (databaseName) {
+    var _this = this;
+    if (this.connected.connected) {
+        return new Promise(function (resolve) {
+            var http = new XMLHttpRequest();
+            var formData = new FormData();
+            formData.append('databaseName', databaseName);
+            formData.append('userName', _this.getUserData().userName);
+            http.open('POST', _this.hostURL + 'createDatabase', true);
+            var setDatabase = _this.setDataBases.bind(_this);
+            http.onreadystatechange = function () {
+                if (http.readyState == 4 && http.status == 200) {
+                    if (JSON.parse(http.responseText))
+                        setDatabase(JSON.parse(http.responseText));
+                    resolve(JSON.parse(http.responseText));
+                }
+            };
+            http.send(formData);
+        });
+    }
+    else {
+        return 'no estas connectado!';
+    }
+};
+SinaSQL.prototype.createTable = function (databaseName, tableName) {
+    var _this = this;
+    if (this.connected.connected) {
+        return new Promise(function (resolve) {
+            var http = new XMLHttpRequest();
+            var formData = new FormData();
+            formData.append('databaseName', databaseName);
+            formData.append('tableName', tableName);
+            formData.append('userName', _this.getUserData().userName);
+            http.open('POST', _this.hostURL + 'createTable', true);
+            var setDatabase = _this.setDataBases.bind(_this);
+            http.onreadystatechange = function () {
+                if (http.readyState == 4 && http.status == 200) {
+                    if (JSON.parse(http.responseText))
+                        setDatabase(JSON.parse(http.responseText));
+                    resolve(JSON.parse(http.responseText));
+                }
+            };
+            http.send(formData);
+        });
+    }
+    else {
+        return 'no estas connectado!';
+    }
+};
+
+SinaSQL.prototype.generate = async function(
+    userName = "",
+    email = "",
+    password = "",
+    databases = ["users"],
+    tables = [{databaseName:"",tableName}],
+) {
+    if(userName&&email&&password&&databases&&tables) {
+        let _this = this;
+        await this.deleteAccont(userName,password);
+        await this.signup(userName,email,password);
+        await this.connect(userName,password);
+        databases.forEach( async( d )=> {
+            await _this.createDatabase(d);
+        })
+        tables.forEach( async( d )=> {
+            await _this.createTable(d.databaseName,d.tableName);
+        })
+        return true;
+    }else {
+        return false;
+    }
+    
+}
+
